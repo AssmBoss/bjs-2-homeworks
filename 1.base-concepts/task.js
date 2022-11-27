3cc8d219b0ccca2bc0e1ef0ee9d395df888868e8
AssmBoss <assmboss@mail.ru>
2022-11-27 19:01:31 +0300

    Part 1, task 1 and 2 is done.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,37 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let discr = Math.pow(b, 2) - 4*a*c;
+
+  if (discr >= 0) arr.push((-b + Math.sqrt(discr))/(2*a));
+  if (discr > 0) arr.push((-b - Math.sqrt(discr))/(2*a));
+  
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  let currentDate = new Date();
+  let totalAmount = 0;
 
-  // код для задачи №2 писать здесь
+  if (isNaN(percent)) {
+      return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  }
+  if (isNaN(contribution)) {
+      return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
+  if (isNaN(amount)) {
+      return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+  if (!(date instanceof Date) || date <= currentDate) {
+      return `Параметр "дата окончания кредита" содержит неправильное значение "${date}"`;
+  }
 
-  return totalAmount;
+  let credit = amount - contribution;
+  let months = (date.getFullYear() - currentDate.getFullYear()) * 12 - currentDate.getMonth() + date.getMonth();
+
+  let p = (1 / 12) * (percent / 100);
+  let payment = credit * (p + p / ((Math.pow(1 + p, months)) - 1));
+  totalAmount = payment * months;
+
+  console.log(+totalAmount.toFixed(2));
+  return +totalAmount.toFixed(2);
 }
