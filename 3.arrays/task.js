91a0eeeb56974aca413b5033a2866274776cbd56
AssmBoss <assmboss@mail.ru>
2022-12-03 19:47:00 +0300

    Part 3, tasks 1,2 is done.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    return arr1.every((element1, index1)=> element1 === arr2[index1]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users.length === 0 ||(gender !== "мужской" && gender !== "женский" )) {
+        return 0;
+    }
+    let genderUsers = users.filter(user => user.gender === gender);
+    return genderUsers.reduce((acc, user, genderUsers) => (acc + user.age), 0)/genderUsers.length;
 }
