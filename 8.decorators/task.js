cf2098fe949ac911a39de2ab51d1800e4754116d
AssmBoss <assmboss@mail.ru>
2022-12-18 02:37:21 +0300

    Part 8, tasks 1,2 is done.

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,43 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let casheArray = [];  
+  return (...args) => {
+    const hash = md5(args.join(','));
+    console.log(hash);
+    for (cache of casheArray) {
+        if (hash in cache) {
+            return "Из кэша: " + cache[hash];
+        }
+    }
+    if (casheArray.length === 5) {
+        casheArray.shift();
+    }
+    const result = func(...args);
+    let newCache = {};
+    newCache[hash] = result;
+    casheArray.push(newCache);
+    return "Вычисляем: " + newCache[hash];
+  }
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    wrapper.allCount = 0;
+    wrapper.count = 0;
+    timeoutId = null;
+    function wrapper(...args) {
+        wrapper.allCount++;
+        if (timeoutId === null && wrapper.count === 0) {
+            func(...args);
+            wrapper.count++;
+        } else if (timeoutId) { 
+            clearTimeout (timeoutId) ;
+        }
+        timeoutId = setTimeout(() => {
+            timeoutId = null;
+            func(...args);
+            wrapper.count++;
+        }, delay);
+    }
+    return wrapper;
 }
